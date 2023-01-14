7e45b45301343b081ee37806a15880490002ff25
fjwxdu <72540512+fjwxdu@users.noreply.github.com>
2023-01-15 00:15:46 +0800

    Create def.hpp

---
+++ def.hpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+#include "./include/def.hpp"
+
+double def(int fx,int fy){
+	double fact=1.0;
+	int i;
+	if(fx==fy)
+		return 1.0;
+	else if(fx>fy){
+		for(i=1;i<=fx-fy;i++)
+			fact=fact*(fy+i);
+		return fact;
+	}
+	else if(fx<fy){
+		for(i=1;i<=fy-fx;i++)
+			fact=fact*(fx+i);
+		return 1.0/fact;
+	}
+}
